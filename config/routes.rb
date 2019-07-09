dd1e0356334579b72b4293b2b86ca3f0607a0a3a
Lisa Chinn <lisamchinn@gmail.com>
2019-07-09 13:13:19 -0300

    finished rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Inventing the CRUD ourting (in the head of DHH)
+  # simplest request is a get
+
+  # read all tasks
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # could have also been list
+  get 'tasks/new', to: 'tasks#new', as: :new
+
+  # read one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+    # create one task (2 requests)
+    # first request is to get (display?) the empty form to write the details about the restaurant
+    # second request - when you submit the form, you make a request to post the
+    # params and retrieve them in the controller and save in the database
+
+  post 'tasks', to: 'tasks#create'
+
+  # update one task (2 requests)
+    # get the edit form
+    # post the params again and actually update in the database
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+    # this is just a stupid action to render the form
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete one task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
